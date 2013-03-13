6d2cd4fbee66c750d5e5095e34432f099b80be3f
Yusuke Suzuki <yusuke@geekfield.jp>
2013-03-13 17:47:35 +0900

    dft_idftのREADME.rstを記述、コメントを記入
    - README.rst に説明と動かし方を記述
    - main.cpp にコメントを記述。不要なincludeを削除。

--- cpp_samples/dft_idft/main.cpp
+++ cpp_samples/dft_idft/main.cpp
@@ -1,5 +1,3 @@
-#include <opencv2/contrib/contrib.hpp>
-
 #include <opencv2/core/core.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
 #include <opencv2/highgui/highgui.hpp>
@@ -7,40 +5,80 @@
 #include <iostream>
 #include <string>
 
+// DFTのための複素画像を作成する
 void create_complex_dft_image(const cv::Mat& in, cv::Mat& out)
 {
 	using namespace cv;
 
-	Mat padded;
+	Mat real_image; // 複素画像の実部
 
-	copyMakeBorder(in, padded,
+	/**
+	 * DFTを行うには画像の両辺それぞれがDFTに適したサイズであることが必要。
+	 * 入力画像をDFTに適したサイズに拡大してコピーした上であまりを塗りつぶす。
+	 *
+	 * cv::getOptimalDFTSize()はDFT入力された大きさに対してDFTに適した大きさを返す。
+	 *
+	 * cv::copyMakeBorder()は入力画像の周囲を指定幅分だけ塗りつぶして出力画像に
+	 * コピーする。
+	 *
+	 * 今回は、DFTのサイズに拡大する分だけ右端と下端を0で塗りつぶしている。
+	 */
+	copyMakeBorder(in, real_image,
 		0, getOptimalDFTSize(in.rows) - in.rows,
 		0, getOptimalDFTSize(in.cols) - in.cols,
 		BORDER_CONSTANT, Scalar::all(0) );
 
-	Mat planes[] = {Mat_<float>(padded), Mat::zeros(padded.size(), CV_32F)};
-
+	// 0で埋めた虚部とマージして複素画像にする。
+	Mat planes[] = {Mat_<float>(real_image), Mat::zeros(real_image.size(), CV_32F)};
 	merge(planes, 2, out);
 }
 
+// DFTの結果である複素画像を表示用の強度画像に変換する。
 void create_fourier_magnitude_image_from_complex(const cv::Mat& in, cv::Mat& out)
 {
 	using namespace cv;
 
+	/**
+	 * 複素画像の実部と虚部を2枚の画像に分離する。
+	 */
 	Mat planes[2];
 	split(in, planes);
 
+	/**
+	 * cv::magnitude()で二枚の画像から強度を算出する。
+	 *
+	 * 強度画像の各ピクセル値の擬似コード
+	 * dst(x,y) = sqrt(pow( src1(x,y),2) + pow(src2(x,y),2) )
+	 */
 	magnitude(planes[0], planes[1], out);
 
+	/**
+	 * 表示用に対数に変換する。そのため各ピクセルに1を加算しておく。
+	 */
 	out += Scalar::all(1);
-
 	log(out, out);
 
-	out = out(
-		Rect(0, 0, out.cols & -2, out.rows & -2) );
+	/**
+	 * 以下に続く入れ替えのために、画像の両辺のサイズを偶数にしておく
+	 */
+	out = out( Rect(0, 0, out.cols & -2, out.rows & -2) );
 
-	normalize(out, out, 0, 1, CV_MINMAX);
 
+	/**
+	 * cv::dft()で得られる画像を一般的な二次元DFTの表示様式にする。
+	 *
+	 * 具体的には以下のように画像の各四半分を入れ替える。
+	 *
+	 * +----+----+    +----+----+
+	 * |    |    |    |    |    |
+	 * | q0 | q1 |    | q3 | q2 |
+	 * |    |    |    |    |    |
+	 * +----+----+ -> +----+----+
+	 * |    |    |    |    |    |
+	 * | q2 | q3 |    | q1 | q0 |
+	 * |    |    |    |    |    |
+	 * +----+----+    +----+----+
+	 */
 	const int half_width = out.cols / 2;
 	const int half_height = out.rows / 2;
 
@@ -61,17 +99,26 @@ void create_fourier_magnitude_image_from_complex(const cv::Mat& in, cv::Mat& out
 	q1.copyTo(tmp);
 	q2.copyTo(q1);
 	tmp.copyTo(q2);
+
+	// 濃淡を強調するために、画像最小値を0に、最大値を1にするように正規化する。
+	normalize(out, out, 0, 1, CV_MINMAX);
 }
 
+// 逆DFTで得られた画像を実画像に変換する。
 void create_inverse_fourier_image_from_complex(
 	const cv::Mat& in, const::cv::Mat& origin, cv::Mat& out)
 {
 	using namespace cv;
 
+	// 複素画像の実部と虚部を2枚の画像に分離する。
 	Mat splitted_image[2];
-
 	split(in, splitted_image);
 
+	/**
+	 * 実部について正規化を行う。
+	 * 入力画像のサイズはDFT用に拡大されているので、原画像の同サイズにROIを設定して
+	 * 縮小する。
+	 */
 	splitted_image[0](cv::Rect(0, 0, origin.cols, origin.rows)).copyTo(out);
 	cv::normalize(out, out, 0, 1, CV_MINMAX);
 }
@@ -97,14 +144,26 @@ int main(int argc, char* argv[])
 	cv::Mat complex_image;
 	create_complex_dft_image(image, complex_image);
 
-	// フーリエ変換
+	/**
+	 * フーリエ変換
+	 *
+	 * 複素画像から複素画像へ変換する。ここでは入力画像自体を出力先にしている。
+	 *
+	 * 呼び出し時のオプションにより複素画像から実画像への変換などもできる。
+	 */
 	cv::dft(complex_image, complex_image);
 
 	// フーリエ変換の結果の可視化
 	cv::Mat magnitude_image;
 	create_fourier_magnitude_image_from_complex(complex_image, magnitude_image);
 
-	// 逆フーリエ変換
+	/**
+	 * 逆フーリエ変換
+	 *
+	 * 複素画像から複素画像へ変換する。ここでは入力画像自体を出力先にしている。
+	 *
+	 * 呼び出し時のオプションにより複素画像から実画像への変換などもできる。
+	 */
 	cv::idft(complex_image, complex_image);
 
 	// 逆フーリエ変換の結果の可視化
@@ -123,7 +182,11 @@ int main(int argc, char* argv[])
 
 	cv::waitKey(0);
 
-	// 結果画像の保存
+	/**
+	 * 結果画像の保存
+	 *
+	 * 表示画像は浮動小数表現で値域が[0,1]なので255を掛けたものを入力とする。
+	 */
 	cv::imwrite(std::string(argv[1]) + ".dft.jpg", magnitude_image * 255);
 	cv::imwrite(std::string(argv[1]) + ".idft.jpg", idft_image * 255);
 
